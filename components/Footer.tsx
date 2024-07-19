62f53d8dc145e4493eb7eb034cc572557f3948bf
Isaiah McNealy <53882361+isaiahmcnealy@users.noreply.github.com>
2024-07-19 18:36:55 -0400

    update social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,60 @@
 // components/Footer.tsx
 import React from 'react';
+import { FaTwitter, FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa';
 
-const Footer = () => (
+const Footer:React.FC = () => (
   <footer className="flex flex-col items-center py-6 bg-gray-900 text-white">
     <ul className="flex items-center" aria-label="Social media">
       <li className="mr-5 text-xs shrink-0">
         <a
           className="block hover:text-slate-200"
-          href="https://linkedin.com/in/yourusername"
+          href="https://linkedin.com/in/isaiahmcnealy"
           target="_blank"
           rel="noreferrer noopener"
           aria-label="LinkedIn (opens in a new tab)"
           title="LinkedIn"
         >
           <span className="sr-only">LinkedIn</span>
-          <i className="fab fa-linkedin h-6 w-6" aria-hidden="true"></i>
+          <FaLinkedin className="text-2xl hover:text-teal-500 transition-colors duration-300"/>
+        </a>
+      </li>
+      <li className="mr-5 text-xs shrink-0">
+        <a
+          className="block hover:text-slate-200"
+          href="https://twitter.com/mcnealy_isaiah"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Twitter (opens in a new tab)"
+          title="Twitter"
+        >
+          <span className="sr-only">LinkedIn</span>
+          <FaTwitter className="text-2xl hover:text-teal-500 transition-colors duration-300" />
+        </a>
+      </li>
+      <li className="mr-5 text-xs shrink-0">
+        <a
+          className="block hover:text-slate-200"
+          href="https://instagram.com/isaiah_mcnealy"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Instagram (opens in a new tab)"
+          title="Instagram"
+        >
+          <span className="sr-only">LinkedIn</span>
+          <FaInstagram className="text-2xl hover:text-teal-500 transition-colors duration-300" />
+        </a>
+      </li>
+      <li className="mr-5 text-xs shrink-0">
+        <a
+          className="block hover:text-slate-200"
+          href="https://github.com/isaiahmcnealy"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="GitHub (opens in a new tab)"
+          title="GitHub"
+        >
+          <span className="sr-only">LinkedIn</span>
+          <FaGithub className="text-2xl hover:text-teal-500 transition-colors duration-300" />
         </a>
       </li>
     </ul>
